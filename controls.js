6973b83552fc49a0aaf48802651668ed15fe5fa5
NelsonChizzy <127169051+NelsonChizzy@users.noreply.github.com>
2023-03-08 00:21:28 +0100

    Add files via upload

---
+++ controls.js
@@ -0,0 +1,6 @@
+const message = ()=>{
+    location.replace("https://api.whatsapp.com/send?phone=9072801629&text=Hello!%20Vicky's%20Cake%20and%20Event%20Deco,%20I%20saw%20your%20products%20online%20and%20I%20want%20to%20place%20an%20order.");
+}
+const contact = ()=>{
+    location.replace("#contactpage");
+}
